a1728d142ee86a354a1b0cb4ddb8adad0cc93b50
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-04-20 13:33:05 +0300

    Submit and Reset Form Part 1

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // 🥰🤠🥳🥳💯💯
+
+document
+  .querySelector("#ewallet-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    // console.log(e);
+    // console.log("Form Submitted");
+
+    const type = document.querySelector(".add__type").value;
+    const desc = document.querySelector(".add__description").value;
+    const value = document.querySelector(".add__value").value;
+    console.log(type, desc, value);
+
+    const newHtml = `
+    <div class="item">
+      <div class="item-description-time">
+        <div class="item-description">
+          <p>${desc}</p>
+        </div>
+        <div class="item-time">
+          <p>25 Feb, 06:45 PM</p>
+        </div>
+      </div>
+      <div class="item-amount expense-amount">
+        <p>${type}$${value}</p>
+      </div>
+    </div>
+    `;
+
+    //console.log(newHtml);
+    // document.querySelector(".collection").innerHTML = innerHtml; // change all html
+    const collection = document.querySelector(".collection");
+    collection.insertAdjacentHTML("afterbegin", newHtml);
+  });
