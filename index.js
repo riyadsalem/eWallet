f8264812a2ca753137c8843d2295897de055a526
Riyad Salem <riyad.m.salem.19988@gmail.com>
2022-04-20 14:02:42 +0300

    Submit and Reset Form Part 2

--- index.js
+++ index.js
@@ -10,26 +10,42 @@ document
     const type = document.querySelector(".add__type").value;
     const desc = document.querySelector(".add__description").value;
     const value = document.querySelector(".add__value").value;
-    console.log(type, desc, value);
+    // console.log(type, desc, value);
 
-    const newHtml = `
-    <div class="item">
-      <div class="item-description-time">
-        <div class="item-description">
-          <p>${desc}</p>
-        </div>
-        <div class="item-time">
-          <p>25 Feb, 06:45 PM</p>
-        </div>
-      </div>
-      <div class="item-amount expense-amount">
-        <p>${type}$${value}</p>
-      </div>
-    </div>
-    `;
-
-    //console.log(newHtml);
-    // document.querySelector(".collection").innerHTML = innerHtml; // change all html
-    const collection = document.querySelector(".collection");
-    collection.insertAdjacentHTML("afterbegin", newHtml);
+    if (desc && value.length > 0) {
+      addItems(desc, type, value);
+      resetForm();
+    }
   });
+
+function addItems(desc, type, value) {
+  const collection = document.querySelector(".collection");
+
+  const newHtml = `
+      <div class="item">
+        <div class="item-description-time">
+          <div class="item-description">
+            <p>${desc}</p>
+          </div>
+          <div class="item-time">
+            <p>25 Feb, 06:45 PM</p>
+          </div>
+        </div>
+        <div class="item-amount ${
+          type === "-" ? "expense-amount" : "income-amount"
+        }">
+          <p>${type}$${value}</p>
+        </div>
+      </div>
+      `;
+
+  //console.log(newHtml);
+  // document.querySelector(".collection").innerHTML = innerHtml; // change all html
+  collection.insertAdjacentHTML("afterbegin", newHtml);
+}
+
+function resetForm() {
+  document.querySelector(".add__type").value = "+";
+  document.querySelector(".add__description").value = "";
+  document.querySelector(".add__value").value = "";
+}
